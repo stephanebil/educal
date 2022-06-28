93a6eb260f1d68111b6ffb72e9ebabe37f64223f
stephanebil <stephanebilella@yahoo.fr>
2022-06-28 16:42:24 +0200

    homesection 3 progress

--- src/components/homePage/HomeCard3.js
+++ src/components/homePage/HomeCard3.js
@@ -5,7 +5,7 @@ export default function HomeCard3({
   return (
     <div className="relative">
       <img src={src} alt="image homesection3" className="w-96 rounded  z-0" />
-      <div className="bg-black w-96 h-60 absolute z-20 top-0 rounded opacity-50 "></div>
+      <div className="bg-black w-96 h-full absolute z-20 top-0 rounded opacity-50 "></div>
     </div>
   );
 }
